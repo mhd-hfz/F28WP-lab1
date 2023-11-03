c87e6dc217260b72e9f4668b6f9bc468ffcf1344
Mohamed Hafeez <mm2170@hw.ac.uk>
2023-11-03 10:22:44 +0400

    Updated

--- week 4/main.js
+++ week 4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.open('GET', 'https://mhd-hfz.github.io/F28WP-lab1/week%204/index.html');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 console.log(ourData[0]);
