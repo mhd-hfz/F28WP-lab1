12c5a722ad5312441af2bc41ee22db73826a7efa
Mohamed Hafeez <mm2170@hw.ac.uk>
2023-11-03 10:19:32 +0400

    Using the JSON.parse()

--- week 4/main.js
+++ week 4/main.js
@@ -1,6 +1,7 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://mhd-hfz.github.io/F28WP-lab1/week%204/cities1.json');
+ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
+var ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
 };
 ourRequest.send();
