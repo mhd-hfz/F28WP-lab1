0b710320423f15e1b053ebf52dbc50d427688fac
Mohamed Hafeez <mm2170@hw.ac.uk>
2023-11-03 09:35:20 +0400

    Working html

---
+++ week 4/main.js
@@ -0,0 +1,14 @@
+var myCities = [
+    {
+    "name": "Dubai",
+    "country": "UAE",
+    "place": "Dubai Mall"
+    },
+    {
+    "name": "Milan",
+    "country": "Italy",
+    "place": "Duomo Di Milano"
+    }
+    ];
+    
+    console.log(myCities[0].name);
