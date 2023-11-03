a261ee14e14d6f0b4f01887cd44de42f2d4cafd7
Mohamed Hafeez <mm2170@hw.ac.uk>
2023-11-03 10:26:05 +0400

    Updated

--- week 4/main.js
+++ week 4/main.js
@@ -1,7 +1,10 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://mhd-hfz.github.io/F28WP-lab1/week%204/index.html');
+ourRequest.open('GET', 'https://mhd-hfz.github.io/F28WP-lab1/week%204/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 console.log(ourData[0]);
 };
 ourRequest.send();
+})
